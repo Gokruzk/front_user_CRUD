538380116b78449e7affed2bc7c12cbebc414914
Gokruzk <nigell@gmail.com>
2024-05-13 21:26:21 -0500

    types

--- types/index.ts
+++ types/index.ts
@@ -3,12 +3,14 @@ import { MouseEventHandler } from "react";
 export interface CustomLinkButtonProps {
   title: string;
   href: string;
+  style?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
 export interface CustomButtonProps {
   title: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
+  style?: string;
 }
 
 export interface User {
@@ -17,4 +19,12 @@ export interface User {
   };
   username: string;
   email: string;
+}
+
+export interface UserWithoutId extends Omit<User, "_id"> {}
+
+export interface Params {
+  params: {
+    userId: number;
+  };
 }
