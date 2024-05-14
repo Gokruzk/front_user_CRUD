3ee81de5dce696f59b3efa439b160a87a8730f85
Gokruzk <nigell@gmail.com>
2024-05-13 21:27:02 -0500

    index

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
-import CustomButton from "@/components/CustomLinkButton";
+import CustomLinkButton from "@/components/CustomLinkButton";
 
 export default function Home() {
-  var cors = require("cors");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>User Register</h1>
-      <CustomButton title="User Register" href="/add_users" />
-      <CustomButton title="View users" href="/view_users" />
+      <CustomLinkButton
+        title="User Register"
+        href="/add_users"
+        style="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      />
+      <CustomLinkButton
+        title="View users"
+        href="/view_users"
+        style="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      />
     </main>
   );
 }
