1f23130a63712406c8e0f8c5d7f24f805baf0b1c
Gokruzk <nigell@gmail.com>
2024-05-13 21:26:51 -0500

    services

---
+++ services/user_crud.tsx
@@ -0,0 +1,50 @@
+import { UserWithoutId } from "@/types";
+
+// get users
+export const getUsers = async () => {
+  const res = await fetch("http://127.0.0.1:5001/user");
+  const users = await res.json();
+  return users;
+};
+
+// get user
+export const getUser = async (id: number) => {
+  const res = await fetch(`http://127.0.0.1:5001/user/${id}`);
+  const user = await res.json();
+  return user;
+};
+
+export const addUser = async ({ username, email }: UserWithoutId) => {
+  const options = {
+    method: "POST",
+    body: JSON.stringify({ username, email }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const res = await fetch("http://127.0.0.1:5001/user", options);
+
+  if (res.ok) {
+    alert("User added successfully");
+  } else {
+    alert("Error while adding user");
+  }
+};
+
+// delete
+export const deleteUser = async (id: number) => {
+  const options = {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const res = await fetch(`http://127.0.0.1:5001/user/${id}`, options);
+  if (res.ok) {
+    alert("User deleted");
+  } else {
+    alert("Error while deleting user");
+  }
+};
