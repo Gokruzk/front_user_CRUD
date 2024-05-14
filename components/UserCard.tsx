d99223263c77ba93008beeb4135eea7fcb9b383e
Gokruzk <nigell@gmail.com>
2024-05-13 21:26:43 -0500

    user card components

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,33 +1,16 @@
 import { User } from "@/types";
 import React from "react";
-import CustomButton from "./CustomButton";
 
 const UserCard = ({ user }: { user: User }) => {
   return (
-    <div className="rounded overflow-hidden p-1 w-80">
-      <div className="p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {user.username}
-          </h5>
-        </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          {user.email}
-        </p>
-        <a
-          href="#"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-        >
-          Update
-        </a>
-        <a
-          href="#"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
-        >
-          Delete
-        </a>
-      </div>
-    </div>
+    <>
+      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        {user.username}
+      </h5>
+      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        {user.email}
+      </p>
+    </>
   );
 };
 
